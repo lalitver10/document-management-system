69dc01f199be02506334e5e66de95edadcf6f2d9
Lalit Verma <lalitver10@gmail.com>
2021-06-14 11:10:21 +0530

    Added ui code

---
+++ document-management-system-ui/src/app/document-report/document-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-report',
+  templateUrl: './document-report.component.html',
+  styleUrls: ['./document-report.component.css']
+})
+export class DocumentReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
